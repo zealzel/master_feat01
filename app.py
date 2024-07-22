218c7c14ddea08180a1fed42bf2812aa78a3bac4
zealzel <zealzel@gmail.com>
2024-07-22 11:21:27 +0800

    delte add dumm xxxxx

--- app.py
+++ app.py
@@ -6,9 +6,6 @@ def Feat1(name, message):
     print(f"Feat1: {name}, {message}!")
     print("justdotit 2")
     print("add XXXX")
-    print("dum 1111")
-    print("dum 2222")
-    print("dum 3333")
 
 
 if __name__ == "__main__":
