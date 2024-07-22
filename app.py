862cac7d6b8b1cb66c3ec7042a244f7441825797
dummm1 <zeal00000001@gmail.com>
2024-07-22 02:45:20 +0000

    update Feat1 func

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ import os
 
 
 def Feat1(name, message):
-    print(f"Feat1: {name}, {message!")
+    print(f"Feat1: {name}, {message}!")
 
 
 if __name__ == "__main__":
     print("hello world")
-    feat1("kevin")
+    Feat1("kevin", "HELLO")
